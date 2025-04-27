e13be3a054e6214e7e07b9f24ca9a75651756a96
rocyuan <rocyuan666@163.com>
2025-04-27 20:21:46 +0800

    mybatis-plus 实体类注解

--- src/main/java/top/rocyuan/entity/UserPlus.java
+++ src/main/java/top/rocyuan/entity/UserPlus.java
@@ -20,6 +20,18 @@ Lombok 常用注解
 @Data  相当于加了 @Getter、@Setter、@ToString、@EqualsAndHashCode、@RequiredArgsConstructor
 */
 
+/*
+mybatis-plus 实体类注解
+@TableName 声明表名
+@TableId 声明主键字段 type 指定主键字段类型：
+    IdType.AUTO（数据库自增）
+    IdType.ASSIGN_ID（默认的，雪花算法生成唯一 ID）
+    IdType.ASSIGN_UUID（使用 UUID 生成唯一 ID）
+    IdType.NONE（未设置主键类型，MyBatis-Plus 不会主动处理主键生成，需要自己手动设置主键值）
+    IdType.INPUT（手动输入，插入数据前要手动设置主键值，MyBatis-Plus 会使用你设置的值作为主键。）
+@TableField 声明其他字段
+*/
+
 @Data
 @TableName("roc_user")
 public class UserPlus {
